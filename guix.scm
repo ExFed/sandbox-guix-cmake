2dc7a72c0c3e198463f1e1e2842f11b3b79fb17a
David Allen <david.allen@columnzero.com>
2024-11-09 14:57:08 -0500

    Derive version from git repo tags

--- guix.scm
+++ guix.scm
@@ -1,13 +1,19 @@
-(use-modules (guix)
-             (guix build-system cmake)
-             (guix packages)
-             (guix licenses))
+(use-modules
+  (guix)
+  (ice-9 popen)
+  (ice-9 rdelim)
+  (guix build-system cmake)
+  (guix packages)
+  (guix licenses))
 
 (define %source-dir (dirname (current-filename)))
 
+(define %git-describe
+  (read-string (open-pipe "git describe --always --tags --dirty | tr -d '\\n'" OPEN_READ)))
+
 (package
   (name "guix-with-cmake")
-  (version "0.0.1")
+  (version %git-describe)
   (source (local-file %source-dir #:recursive? #t))
   (build-system cmake-build-system)
   (synopsis "Guix with CMake")
